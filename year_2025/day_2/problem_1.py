0546e24b3b3eed57cad21785050e61c1e0282c16
mai <maialenberrondo13@gmail.com>
2025-12-02 11:15:38 +0100

    feat: day two working

--- year_2025/day_2/problem_1.py
+++ year_2025/day_2/problem_1.py
@@ -4,10 +4,10 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 from year_2025.utils import read_sequence
 
-data_path = Path(__file__).with_name("test_sequence.txt")
+data_path = Path(__file__).with_name("sequence.txt")
 sequence_list = read_sequence(str(data_path), delimiter=",")
 
-# Problem 1
+# Part 1
 
 ADDITION_OF_ALL_INVALID_IDS = 0
 
@@ -24,5 +24,24 @@ for id_range in sequence_list:
             ADDITION_OF_ALL_INVALID_IDS += id
 
 
+
+print(ADDITION_OF_ALL_INVALID_IDS)
+ADDITION_OF_ALL_INVALID_IDS = 0
+
+# Part 2
+
+def check_if_a_pattern_exists(id: int, split_index: int) -> bool:
+    parts = [id[i:i+split_index] for i in range(0, len(id), split_index)]
+    return len(set(parts)) == 1
+
+for id_range in sequence_list:
+
+    start, end = id_range.split("-")
+
+    for id in range(int(start), int(end) + 1):
+        for split_index in range(1, len(str(id))):
+            if check_if_a_pattern_exists(str(id), split_index):
+                ADDITION_OF_ALL_INVALID_IDS += id
+                break
 
 print(ADDITION_OF_ALL_INVALID_IDS)
