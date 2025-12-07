db2d2c7c8fd9d20e8255bb7d5c1814e99b2e03c0
mai <maialenberrondo13@gmail.com>
2025-12-07 11:27:08 +0100

    feat: add solution to part 1

--- year_2025/day_6/problem.py
+++ year_2025/day_6/problem.py
@@ -7,15 +7,27 @@ from enum import Enum
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 from year_2025.utils import read_sequence
 
-data_path = Path(__file__).with_name("test_sequence.txt")
+data_path = Path(__file__).with_name("sequence.txt")
 sequence_list = read_sequence(str(data_path), delimiter="\n")
+sequence_list = [x.split() for x in sequence_list]
 
-import pdb; pdb.set_trace()
+OPERATOR_LINE = 4 # Change to 3 for test
 
-OPERATOR_LINE = 3
+TOTAL = 0
+for j in range(len(sequence_list[0])):
+    if sequence_list[OPERATOR_LINE][j] == "*":
+        cnt = 1
+    elif sequence_list[OPERATOR_LINE][j] == "+":
+        cnt = 0
+    for i in range(len(sequence_list) - 1):
+        if sequence_list[OPERATOR_LINE][j] == "*":
+            # print(int(sequence_list[i][j]))
+            cnt *= int(sequence_list[i][j])
+        elif sequence_list[OPERATOR_LINE][j] == "+":
+            # import pdb; pdb.set_trace()
+            # print(sequence_list[i][j])
+            cnt += int(sequence_list[i][j])
+    TOTAL += cnt
 
-class OPERATORS(Enum)
-    
-
-for i in range(len(sequence_list[0])):
-    for j in range(sequence_list):
+print(TOTAL)
+print(TOTAL == 4277556)
